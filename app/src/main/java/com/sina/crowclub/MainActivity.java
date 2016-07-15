92107aadd7da7af6b1752e9dacd178d1131eed94
wuzp <wzp09tjlg@163.com>
2016-07-15 09:26:57 +0800

    add a TAG string to mainActivity class

--- app/src/main/java/com/sina/crowclub/MainActivity.java
+++ app/src/main/java/com/sina/crowclub/MainActivity.java
@@ -5,9 +5,17 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
+    private Button mBtnShow;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         initViews();
     }
+
+    private void initViews(){}
 }
