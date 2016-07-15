fc59662c3b7b0349c3167f81dadac22a87cdba09
wuzp <wzp09tjlg@163.com>
2016-07-15 09:40:15 +0800

    1.添加imort 类文件

--- app/src/main/java/com/sina/crowclub/MainActivity.java
+++ app/src/main/java/com/sina/crowclub/MainActivity.java
@@ -1,7 +1,8 @@
 package com.sina.crowclub;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
