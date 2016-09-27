3f794650feed7a74f28736a540e11d0e083002ce
wuzp <wzp09tjlg@163.com>
2016-09-27 18:03:56 +0800

    1.提交以前添加的modual refreshlayout(好像之后就已经删除了,不知道为哈还需要添加)

---
+++ refreshlayout/src/main/java/com/sina/refreshlayout/RefreshLayout.java
@@ -0,0 +1,19 @@
+package com.sina.refreshlayout;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+/**
+ * Created by wu on 2016/8/13.
+ */
+public class RefreshLayout extends ViewGroup {
+
+    public RefreshLayout(Context context){
+        super(context);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
