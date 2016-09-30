32d1ba7f5a3d0b913218bf4d71d73e9680efea13
wuzp <wzp09tjlg@163.com>
2016-09-30 10:50:54 +0800

    1.添加阿里百川的sdk中的jar包。

---
+++ app/src/main/java/com/sina/crowclub/network/Parse/EmptyBean.java
@@ -0,0 +1,11 @@
+package com.sina.crowclub.network.Parse;
+
+import java.io.Serializable;
+
+/**
+ * Created by wu on 2016/9/28.
+ */
+public class EmptyBean implements Serializable {
+    public int     res_code;
+    public String  res_msg;
+}
